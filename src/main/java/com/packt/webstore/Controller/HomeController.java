918fbeca7244c97b9c468a5ef99148eb1366a6f4
Florek <pawel.florczak87@gmail.com>
2020-04-09 21:28:31 +0200

    Initial commit

---
+++ src/main/java/com/packt/webstore/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.packt.webstore.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("greeting", "Sklep internetowy");
+		model.addAttribute("tagline", "Spring Framework");
+		
+		return "welcome";
+	}
+}
